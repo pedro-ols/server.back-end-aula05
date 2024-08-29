8491ec9519975d135c22adc217d5a23539f79875
Pedro Oliveira <pedro.o.santos7@aluno.senai.br>
2024-08-29 11:22:52 -0300

    feat: add create emotion request

--- src/server.js
+++ src/server.js
@@ -59,6 +59,17 @@ app.get("/emocoes", (req, res) => {
     return res.status(200).send( emocoes )
 })
 
+app.post("/emocoes", (req, res) => {
+    const { nome, cor } = req.body
+    const novaEmocao = {
+        id: emocoes.length + 1,
+        nome: nome,
+        cor: cor
+    }
+    emocoes.push(novaEmocao)
+    return res.status(200).send( emocoes )
+})
+
 app.get("/personagens", (req, res) => {
     return res.status(200).send( personagens )
 })

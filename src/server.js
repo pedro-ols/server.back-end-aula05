454d228b4b1b1a0cd117e0d137d39fed6518e8a3
Pedro Oliveira <pedro.o.santos7@aluno.senai.br>
2024-08-29 10:49:59 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -8,12 +8,59 @@ const serverPort = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
+const emocoes = [
+    {
+        id: 1,
+        nome: "Nojinho",
+        cor: "Verde"
+    },
+    {
+        id: 2,
+        nome: "Medo",
+        cor: "Roxo"
+    },
+    {
+        id: 3,
+        nome: "Alegria",
+        cor: "Amarelo"
+    }
+]
+
+const personagens = [
+    {
+        id: 100,
+        nome: "Woody",
+        studio: "Disney/Pixar",
+        vivo: true
+    },
+    {
+        id: 101,
+        nome: "Shrek",
+        studio: "Dreamworks",
+        vivo: true
+    },
+    {
+        id: 102,
+        nome: "Scooby",
+        studio: "Hanna-Barbera",
+        vivo: true
+    },
+]
+
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!" })
 })
 
 app.get("/2tds2", (req, res) => {
     return res.status(200).send({ message: "Hello, World!" })
+})
+
+app.get("/emocoes", (req, res) => {
+    return res.status(200).send( emocoes )
+})
+
+app.get("/personagens", (req, res) => {
+    return res.status(200).send( personagens )
 })
 
 app.listen(serverPort, () => {

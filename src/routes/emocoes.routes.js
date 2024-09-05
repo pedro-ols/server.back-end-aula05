59f7ea4e2bc32a0949acd5d792ffe892cb595413
Pedro Oliveira <pedro.o.santos7@aluno.senai.br>
2024-09-05 15:39:29 -0300

    create routes configs

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express"
 
 const emocoesRoutes = Router()
 
-const emocoes = [
+let emocoes = [
     {
         id: 1,
         nome: "Nojinho",
@@ -20,17 +20,21 @@ const emocoes = [
     }
 ]
 
-app.get("/emocoes", (req, res) => {
-    return res.status(200).send( emocoes )
-})
+emocoesRoutes.get("/", (req, res) => {
+    return res.status(200).send(emocoes);
+});
 
-app.post("/emocoes", (req, res) => {
-    const { nome, cor } = req.body
+emocoesRoutes.post("/", (req, res) => {
+    const { nome, cor } = req.body;
+
     const novaEmocao = {
         id: emocoes.length + 1,
         nome: nome,
-        cor: cor
-    }
-    emocoes.push(novaEmocao)
-    return res.status(200).send( emocoes )
+        cor: cor,
+    };
+    emocoes.push(novaEmocao);
+
+    return res.status(200).send(emocoes);
 })
+
+export default emocoesRoutes
--- src/server.js
+++ src/server.js
@@ -1,20 +1,16 @@
 import express from "express"
 import {config} from "dotenv"
 
-config()
+import routes from "./routes/index.routes.js";
+
+config();
 
 const serverPort = process.env.PORT || 3000
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+app.use(routes);
 
-app.get("/", (req, res) => {
-    return res.status(200).send({ message: "Hello, World!" })
-})
-
-app.get("/2tds2", (req, res) => {
-    return res.status(200).send({ message: "Hello, World!" })
-})
 
 app.listen(serverPort, () => {
     console.log(`🐧 Server started on http://localhost:${serverPort}`)

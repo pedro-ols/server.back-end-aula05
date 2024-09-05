00a340883ff94ef1853ec1ea3c6011e922a2e691
Pedro Oliveira <pedro.o.santos7@aluno.senai.br>
2024-09-05 17:20:06 -0300

    feat: create get by id method

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -1,6 +1,6 @@
-import { Router } from "express"
+import { Router } from "express";
 
-const emocoesRoutes = Router()
+const emocoesRoutes = Router();
 
 let emocoes = [
     {
@@ -18,12 +18,14 @@ let emocoes = [
         nome: "Alegria",
         cor: "Amarelo"
     }
-]
+];
 
+//Rota para buscar todas as emoções;
 emocoesRoutes.get("/", (req, res) => {
     return res.status(200).send(emocoes);
 });
 
+//Rota para criar uma nova emoção
 emocoesRoutes.post("/", (req, res) => {
     const { nome, cor } = req.body;
 
@@ -35,6 +37,25 @@ emocoesRoutes.post("/", (req, res) => {
     emocoes.push(novaEmocao);
 
     return res.status(200).send(emocoes);
-})
+});
 
-export default emocoesRoutes
+//rota para buscar uma emoção por seu ID
+emocoesRoutes.get("/:id", (req, res) => {
+    const { id } = req.params;
+    
+    //console.log(id);
+
+    const emocao = emocoes.find((emotion) => emotion.id == id);
+    if(!emocao){
+        return res.status(404) .send({
+            message:"emoção não encontrada",
+        });
+    };
+
+    return res.status(200).send({
+        message:"emoção encontrada",
+        emocao,
+    });
+});
+
+export default emocoesRoutes;

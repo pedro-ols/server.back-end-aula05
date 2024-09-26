3c510e677a2ea199f235152baff32eae6462b0fc
Pedro Oliveira <pedro.o.santos7@aluno.senai.br>
2024-09-26 15:06:09 -0300

    create: delete emotion

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -72,6 +72,22 @@ emocoesRoutes.put("/:id", (req, res) => {
     return res.status(200).send({
         message: "Emoção atualizada!", emocao,
     });
-    }); 
+});
 
-    export default emocoesRoutes;
+emocoesRoutes.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    const emocao = emocoes.find((emotion) => emotion.id == id);
+    if (!emocao) {
+        return res.status(404).send({
+            message: "emoção não encontrada",
+        });
+    };
+
+    emocoes = emocoes.filter((emotion) => emotion.id != id);
+
+    return res.status(200).send({
+        message: "Emoção deletada.", emocao,
+    });
+});
+
+export default emocoesRoutes;
